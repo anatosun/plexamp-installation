3a72d7718715bec93eef4b91816df3882d8a38c5
François-Xavier Wicht <wichtfx@protonmail.ch>
2022-12-11 16:06:09 +0100

    feat(nvm): add bash completion after initial install

--- install.sh
+++ install.sh
@@ -20,6 +20,9 @@ fi
 
 if ! [ -x "$(command -v nvm)" ]; then
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
+    export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 fi
 
 cd
